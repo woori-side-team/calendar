fcf8bd1cc0481c1ed1815812adfe7657421c46c0
Avantgarde95 <mathematicianscott@gmail.com>
2022-10-25 10:49:21 +0900

    [feat] Bottom sheet 올라오면 backdrop 보여주기
    * Bottom sheet이 최하단 -> Backdrop 없음
    * Bottom sheet이 최하단보다 위 -> Backdrop 존재

---
+++ lib/common/custom_backdrop.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class CustomBackdrop extends StatelessWidget {
+  final double value;
+  final double minValue;
+  final double maxValue;
+  final double minShowValue;
+
+  const CustomBackdrop(
+      {super.key,
+      required this.value,
+      required this.minValue,
+      required this.maxValue,
+      required this.minShowValue});
+
+  @override
+  Widget build(BuildContext context) {
+    if (value < minShowValue) {
+      return const SizedBox(width: 0, height: 0);
+    } else {
+      final ratio =
+          clampDouble((value - minValue) / (maxValue - minValue), 0, 1);
+
+      return Container(
+          color: Color.fromARGB((ratio * 0.8 * 255).floor(), 144, 144, 144),
+          constraints: const BoxConstraints.expand());
+    }
+  }
+}
--- lib/schedule/schedule_sheet.dart
+++ lib/schedule/schedule_sheet.dart
@@ -1,7 +1,8 @@
+import 'package:calendar/common/custom_backdrop.dart';
 import 'package:calendar/common/custom_theme.dart';
 import 'package:flutter/material.dart';
 
-class ScheduleSheet extends StatelessWidget {
+class ScheduleSheet extends StatefulWidget {
   static const minSize = 0.2;
   static const maxSize = 1.0;
   static final snapSizes = [minSize, 0.5, maxSize];
@@ -11,60 +12,89 @@ class ScheduleSheet extends StatelessWidget {
   const ScheduleSheet({super.key, required this.controller});
 
   @override
+  State<StatefulWidget> createState() {
+    return _ScheduleSheetState();
+  }
+}
+
+class _ScheduleSheetState extends State<ScheduleSheet> {
+  double _sheetSize = ScheduleSheet.minSize;
+
+  void _handleChange() {
+    setState(() {
+      _sheetSize = widget.controller.size;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    widget.controller.addListener(_handleChange);
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_handleChange);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-        controller: controller,
-        minChildSize: minSize,
-        maxChildSize: maxSize,
-        initialChildSize: minSize,
-        snapSizes: snapSizes,
-        snap: true,
-        builder: (sheetContext, scrollController) => Container(
-            decoration: BoxDecoration(
-                color: CustomTheme.background.primary,
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12)),
-                boxShadow: const [
-                  BoxShadow(
-                      offset: Offset(0, -6),
-                      blurRadius: 16,
-                      color: Color.fromARGB(128, 174, 174, 178))
-                ]),
-            child: SingleChildScrollView(
-                controller: scrollController,
-                child: Column(children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 6, bottom: 6),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 40,
-                            height: 6,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15),
-                              color: CustomTheme.gray.gray4,
-                            ),
-                          ),
-                        ]),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 6, left: 20, right: 20),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text('다가오는 일정',
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.w600)),
-                          TextButton(
-                              onPressed: () {},
-                              child: Text('편집',
-                                  style: TextStyle(
-                                      fontSize: 14,
-                                      color: CustomTheme.tint.blue)))
-                        ]),
-                  )
-                ]))));
+    final decoration = BoxDecoration(
+        color: CustomTheme.background.primary,
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(12), topRight: Radius.circular(12)),
+        boxShadow: const [
+          BoxShadow(
+              offset: Offset(0, -6),
+              blurRadius: 16,
+              color: Color.fromARGB(128, 174, 174, 178))
+        ]);
+
+    final handle = Padding(
+      padding: const EdgeInsets.only(top: 6, bottom: 6),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Container(
+          width: 40,
+          height: 6,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: CustomTheme.gray.gray4,
+          ),
+        ),
+      ]),
+    );
+
+    final header = Padding(
+        padding: const EdgeInsets.only(top: 6, left: 20, right: 20),
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          const Text('다가오는 일정',
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)),
+          TextButton(
+              onPressed: () {},
+              child: Text('편집',
+                  style: TextStyle(fontSize: 14, color: CustomTheme.tint.blue)))
+        ]));
+
+    return Stack(children: [
+      CustomBackdrop(
+          value: _sheetSize,
+          minValue: ScheduleSheet.minSize,
+          maxValue: ScheduleSheet.maxSize,
+          minShowValue: ScheduleSheet.minSize + 0.1),
+      DraggableScrollableSheet(
+          controller: widget.controller,
+          minChildSize: ScheduleSheet.minSize,
+          maxChildSize: ScheduleSheet.maxSize,
+          initialChildSize: ScheduleSheet.minSize,
+          snapSizes: ScheduleSheet.snapSizes,
+          snap: true,
+          builder: (sheetContext, scrollController) => Container(
+              decoration: decoration,
+              child: SingleChildScrollView(
+                  controller: scrollController,
+                  child: Column(children: [handle, header]))))
+    ]);
   }
 }
