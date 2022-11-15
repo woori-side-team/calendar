3614640662dca6e555f0dbc1bdba722199d6c9aa
JustNewbieProgrammer <mytpewpew@gmail.com>
2022-11-15 18:37:58 +0900

    main 버그 픽스

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:calendar/app.dart';
 import 'package:calendar/common/di/di.dart';
 import 'package:calendar/data/data_sources/local/schedule_entity.dart';
 import 'package:calendar/presentation/providers/schedules_provider.dart';
-import 'package:calendar/presentation/providers/selection_provider.dart';
 import 'package:calendar/presentation/providers/sheet_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -21,7 +20,6 @@ void main() async {
 
   // 앱 세팅 및 시작.
   runApp(MultiProvider(providers: [
-    ChangeNotifierProvider(create: (context) => SelectionProvider()),
     ChangeNotifierProvider(create: (context) => SheetProvider()),
     ChangeNotifierProvider(create: (context) => getIt<SchedulesProvider>())
   ], child: const App()));
