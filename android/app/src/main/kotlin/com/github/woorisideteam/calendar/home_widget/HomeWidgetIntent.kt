38318297aee066ce4e4c5d3044f40ad3f0ab6906
JustNewbieProgrammer <mytpewpew@gmail.com>
2023-02-15 17:02:06 +0900

    앱바 디자인 통일, aos 홈화면 위젯 실험

---
+++ android/app/src/main/kotlin/com/github/woorisideteam/calendar/home_widget/HomeWidgetIntent.kt
@@ -0,0 +1,44 @@
+package com.github.woorisideteam.calendar.home_widget
+
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+
+object HomeWidgetLaunchIntent {
+
+    const val HOME_WIDGET_LAUNCH_ACTION = "es.antonborri.home_widget.action.LAUNCH"
+
+    fun <T> getActivity(context: Context, activityClass: Class<T>, uri: Uri? = null): PendingIntent where T : Activity {
+        val intent = Intent(context, activityClass)
+        intent.data = uri
+        intent.action = HOME_WIDGET_LAUNCH_ACTION
+
+        var flags = PendingIntent.FLAG_UPDATE_CURRENT
+        if (Build.VERSION.SDK_INT >= 23) {
+            flags = flags or PendingIntent.FLAG_IMMUTABLE
+        }
+
+        return PendingIntent.getActivity(context, 0, intent, flags)
+    }
+}
+
+
+object HomeWidgetBackgroundIntent {
+    private const val HOME_WIDGET_BACKGROUND_ACTION = "es.antonborri.home_widget.action.BACKGROUND"
+
+    fun getBroadcast(context: Context, uri: Uri? = null): PendingIntent {
+        val intent = Intent(context, HomeWidgetBackgroundReceiver::class.java)
+        intent.data = uri
+        intent.action = HOME_WIDGET_BACKGROUND_ACTION
+
+        var flags = PendingIntent.FLAG_UPDATE_CURRENT
+        if (Build.VERSION.SDK_INT >= 23) {
+            flags = flags or PendingIntent.FLAG_IMMUTABLE
+        }
+
+        return PendingIntent.getBroadcast(context, 0, intent, flags)
+    }
+}
